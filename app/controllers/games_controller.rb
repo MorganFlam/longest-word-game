9df11498f4cf7299c9a9d49f0747fadb2e5aac0b
Morgan Flament Wallin <morganflamentwallin@gmail.com>
2021-12-20 17:24:11 +0100

    Created letter cards logic and display

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,12 @@
 class GamesController < ApplicationController
   def new
+    @new_game = []
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
+    10.times do
+      r = rand 0..25
+      @new_game << letters[r].capitalize
+    end
   end
 
   def score
