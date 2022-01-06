9a1e5e2601c42e2c30fabecd684c3aeabd03d74c
Morgan Flament Wallin <morganflamentwallin@gmail.com>
2022-01-06 10:24:48 +0100

    Finished game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @new_game = []
@@ -10,5 +12,21 @@ class GamesController < ApplicationController
   end
 
   def score
+    @letters = params[:letters]
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
